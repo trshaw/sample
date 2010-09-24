409c52d0c3d15570d9dd2df8e6279173b646d004
Tom Shaw <tom@trshaw.com>
2010-09-23 22:18:22 -0500

    Done with Chapter 9

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |u|
+  u.name                   "Tom Shaw"
+  u.email                  "tom@trshaw.com"
+  u.password               "password"
+  u.password_confirmation  "password"
+end
